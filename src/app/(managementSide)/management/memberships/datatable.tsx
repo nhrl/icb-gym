51488afa534c400d004a311f46f4507a6b6a2707
Aris Antonio Co <arisaco@su.edu.ph>
2024-10-16 10:48:04 +0800

    fixed transaction

--- src/app/(managementSide)/management/memberships/datatable.tsx
+++ src/app/(managementSide)/management/memberships/datatable.tsx
@@ -46,13 +46,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-
-import { MembershipRegistration } from "./columns";
+import { MembershipRegistration } from "./columns"; // Adjust import based on your actual file structure
+import { mutate as swrMutate } from "swr"; // Ensure SWR is installed
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
-  mutate: () => void;
+  mutate: () => void; // Revalidate function passed as a prop
 }
 
 export function DataTable<TData, TValue>({
@@ -64,7 +64,8 @@ export function DataTable<TData, TValue>({
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = React.useState({});
-  const [isModalOpen, setIsModalOpen] = useState(false); // Single state to control modal visibility
+
+  const api = process.env.NEXT_PUBLIC_API_URL;
 
   const table = useReactTable({
     data,
@@ -85,70 +86,63 @@ export function DataTable<TData, TValue>({
     },
   });
 
-  const metadata = {
-    title: "Memberships",
-    description: "Manage your memberships",
-  };
-
-  const api = process.env.NEXT_PUBLIC_API_URL;
-  const handleConfirm = async() => {
+  const handleConfirm = async () => {
     const ids = table
-    .getSelectedRowModel()
-    .rows.map((row) => (row.original as MembershipRegistration).membership_rid); 
+      .getSelectedRowModel()
+      .rows.map((row) => (row.original as MembershipRegistration).membership_rid);
+
     try {
       const response = await fetch(`${api}/api/manager/transaction/membership`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ ids }), // Send the IDs to the server as JSON
-      })
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ids }),
+      });
 
       const result = await response.json();
-      if(result.success) {
-        mutate(); // Assuming mutate refreshes the data or triggers necessary action
+      if (result.success) {
+        console.log(result.message);
+        swrMutate(`${api}/api/manager/transaction/membership`); // Revalidate data
+        mutate(); // Trigger the parent mutate function if needed
       } else {
-        //error message here
+        console.error(result.message);
       }
     } catch (error) {
       console.error("An error occurred during confirmation: ", error);
-      // Handle the error, possibly display it to the user
     }
   };
 
   const handleDelete = async () => {
     const ids = table
       .getSelectedRowModel()
-      .rows.map((row) => (row.original as MembershipRegistration).membership_rid); // Cast to MembershipRegistration
+      .rows.map((row) => (row.original as MembershipRegistration).membership_rid);
+
     try {
       const response = await fetch(`${api}/api/manager/transaction/cancelMembership`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ ids }), // Send the IDs to the server as JSON
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ids }),
       });
 
       const result = await response.json();
       if (result.success) {
-        console.log("Successfully deleted membership: ", result);
+        console.log("Successfully deleted membership:", result);
+        swrMutate(`${api}/api/manager/transaction/membership`);
         mutate();
       } else {
-        console.error("Failed to delete membership: ", result.message);
-        // Handle the error, possibly display it to the user
+        console.error("Failed to delete membership:", result.message);
       }
     } catch (error) {
       console.error("An error occurred during deletion: ", error);
-      // Handle the error, possibly display it to the user
     }
   };
-  
 
   return (
     <div className="flex flex-col gap-4">
       <div>
-        <h1 className="font-black text-2xl">{metadata.title}</h1>
-        <p className="font-normal text-sm text-muted-foreground">{metadata.description}</p>
+        <h1 className="font-black text-2xl">Memberships</h1>
+        <p className="font-normal text-sm text-muted-foreground">
+          Manage your memberships
+        </p>
       </div>
 
       <div className="flex sm:flex-row flex-col items-center gap-2">
@@ -172,49 +166,41 @@ export function DataTable<TData, TValue>({
               {table
                 .getAllColumns()
                 .filter((column) => column.getCanHide())
-                .map((column) => {
-                  return (
-                    <DropdownMenuCheckboxItem
-                      key={column.id}
-                      className="capitalize"
-                      checked={column.getIsVisible()}
-                      onCheckedChange={(value) => column.toggleVisibility(!!value)}
-                    >
-                      {column.id}
-                    </DropdownMenuCheckboxItem>
-                  );
-                })}
+                .map((column) => (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                ))}
             </DropdownMenuContent>
           </DropdownMenu>
 
-          {/* Confirm Membership Button */}
-          <div className="flex flex-col gap-4">
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button
-                  variant="secondary"
-                  className="flex ml-auto flex-row gap-2"
-                >
-                  <CheckCircleIcon className="h-4 w-4" />
-                  Confirm Membership
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Confirm Membership?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Are you sure you want to confirm this membership? This action cannot be undone.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction onClick={handleConfirm}>Confirm</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="secondary" className="flex ml-auto flex-row gap-2">
+                <CheckCircleIcon className="h-4 w-4" />
+                Confirm Membership
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Confirm Membership?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Are you sure you want to confirm this membership? This action
+                  cannot be undone.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleConfirm}>Confirm</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
 
-          {/* Delete Button */}
           <AlertDialog>
             <AlertDialogTrigger className="border border-border p-2 rounded-sm hover:bg-muted">
               <TrashIcon className="h-4 w-4" />
@@ -223,7 +209,7 @@ export function DataTable<TData, TValue>({
               <AlertDialogHeader>
                 <AlertDialogTitle>Are you sure you want to delete this?</AlertDialogTitle>
                 <AlertDialogDescription>
-                  This action cannot be undone. This will permanently remove your data from the database.
+                  This action cannot be undone. This will permanently remove your data.
                 </AlertDialogDescription>
               </AlertDialogHeader>
               <AlertDialogFooter>
@@ -240,20 +226,17 @@ export function DataTable<TData, TValue>({
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
-                    </TableHead>
-                  );
-                })}
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {!header.isPlaceholder &&
+                      flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                ))}
               </TableRow>
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length ? (
+            {table.getRowModel().rows.length ? (
               table.getRowModel().rows.map((row) => (
                 <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
                   {row.getVisibleCells().map((cell) => (
